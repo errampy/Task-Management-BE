9ec149f4d91ae36e0f3a19a244b8487eac52370e
errampy <rampython2019@gmail.com>
2025-06-24 00:37:32 +0300

    added the mysql db connectivity

--- task_manager_pro/settings.py
+++ task_manager_pro/settings.py
@@ -78,12 +78,26 @@ WSGI_APPLICATION = 'task_manager_pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# ========== user for development =============
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# ================= MySqlDbConnectivity ===============
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # MySQL backend
+        'NAME': 'taskmanager',
+        'USER': 'ram',
+        'PASSWORD': '112233',
+        'HOST': 'localhost',   # or your remote DB host
+        'PORT': '3306',        # default MySQL port
     }
 }
+
 
 
 # Password validation
