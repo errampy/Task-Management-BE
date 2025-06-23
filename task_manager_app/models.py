c4816afcc30968d7796cf8cf4aaa004dcb04afe0
errampy <rampython2019@gmail.com>
2025-06-23 23:22:31 +0300

    finish the developement

--- task_manager_app/models.py
+++ task_manager_app/models.py
@@ -30,8 +30,8 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # class Meta:
-    #     ordering = ['-created_at']
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
